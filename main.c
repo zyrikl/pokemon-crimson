aa29701ddbf27cf3a0188ab2979b25aec33d016f
Charles Wang <140123838+zyrikl@users.noreply.github.com>
2025-05-10 18:29:07 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char name[256];
+
+    printf("Presenting...!\n");
+    printf("Pokemon Red\n");
+    printf("Created by Zyrikl 8-bit Studios\n");
+
+    printf("What's your name? ");
+    scanf("%s", name);
+    printf("Well, hello %s", name);
+    printf(".");
+}
